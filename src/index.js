e2d81be7359773a0d4b2bf007edb72e250b30f64
Susanne Öberg <Susanne-Oberg@users.noreply.github.com>
2023-02-16 19:47:24 +0200

    added javascript date and time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  let helsinkiElement = document.querySelector("#helsinki");
+  let helsinkiDateElement = helsinkiElement.querySelector(".date");
+  let helsinkiTimeElement = helsinkiElement.querySelector(".time");
+  let helsinkiTime = moment().tz("Europe/Helsinki");
+  helsinkiDateElement.innerHTML = helsinkiTime.format("MMMM Do YYYY");
+  helsinkiTimeElement.innerHTML = helsinkiTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
